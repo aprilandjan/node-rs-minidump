15b046247b97ec6a9847181105f30be1f221bb16
merlin.ye <merlin.ye@qq.com>
2023-11-09 11:31:14 +0800

    chore: improve combined publish script

--- prepare-combined.js
+++ prepare-combined.js
@@ -4,24 +4,33 @@ const fs = require('fs-extra')
 
 const pkg = require('./package.json')
 
-const publishPath = path.join(__dirname, './npm-combined')
+const targetsPath = path.join(__dirname, './npm')
+const combinedPath = path.join(__dirname, './npm-combined')
 
 // create the temp publish folder
-fs.ensureDirSync(publishPath)
-fs.emptyDirSync(publishPath)
+fs.ensureDirSync(combinedPath)
+fs.emptyDirSync(combinedPath)
 
 // modify pkg name & content
 pkg.name = pkg.name + '-combined'
-pkg.dependencies = {
-  ...pkg.optionalDependencies,
-}
 pkg.optionalDependencies = {}
 pkg.devDependencies = {}
 pkg.scripts = {}
 
-// write needed file
-fs.writeJsonSync(path.join(publishPath, 'package.json'), pkg)
-fs.copyFileSync(path.join(__dirname, 'index.js'), path.join(publishPath, 'index.js'))
-fs.copyFileSync(path.join(__dirname, 'index.d.ts'), path.join(publishPath, 'index.d.ts'))
-fs.copyFileSync(path.join(__dirname, 'LICENSE'), path.join(publishPath, 'LICENSE'))
-fs.copyFileSync(path.join(__dirname, 'README-combined.md'), path.join(publishPath, 'README.md'))
+// copy needed files
+fs.writeJsonSync(path.join(combinedPath, 'package.json'), pkg)
+fs.copyFileSync(path.join(__dirname, 'index.js'), path.join(combinedPath, 'index.js'))
+fs.copyFileSync(path.join(__dirname, 'index.d.ts'), path.join(combinedPath, 'index.d.ts'))
+fs.copyFileSync(path.join(__dirname, 'LICENSE'), path.join(combinedPath, 'LICENSE'))
+fs.copyFileSync(path.join(__dirname, 'README-combined.md'), path.join(combinedPath, 'README.md'))
+
+// copy each target binaries
+const targets = fs.readdirSync(targetsPath)
+targets.forEach((target) => {
+  const targetPkg = path.join(targetsPath, target, './package.json')
+  const mainFileName = fs.readJsonSync(targetPkg).main
+  const mainFilePath = path.join(targetsPath, target, mainFileName)
+  if (fs.existsSync(mainFilePath)) {
+    fs.copyFileSync(mainFilePath, path.join(combinedPath, mainFileName))
+  }
+})
