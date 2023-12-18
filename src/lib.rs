5accdb0b230b5ed3deca4dbbeb76dc0b91b441c6
aprilandjan <merlin.ye@qq.com>
2023-12-18 23:23:48 +0800

    feat: add system info

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(clippy::all)]
-use std::collections::HashMap;
+use std::{collections::HashMap, alloc::System};
 use napi_derive::napi;
 use memmap2::Mmap;
 
@@ -18,76 +18,9 @@ pub struct CrashpadInfo {
   pub module_list: Vec<MinidumpModuleCrashpadInfo>,
 }
 
-/// Get crashpad-specific information carried within a minidump file.
-/// For example: process type, process pid, etc.
-#[napi]
-pub fn get_crashpad_info(path: String) -> napi::Result<CrashpadInfo> {
-  // Read the minidump from a file
-  let dump_result = minidump::Minidump::read_path(path);
-
-  let dump = match dump_result {
-    Ok(dump) => dump,
-    Err(_) => {
-      return Err(napi::Error::new(
-        napi::Status::InvalidArg,
-        "read minidump file failed".to_owned(),
-      ))
-    }
-  };
-
-  let crashpad_info_result = dump.get_stream::<minidump::MinidumpCrashpadInfo>();
-
-  let crashpad_info = match crashpad_info_result {
-    Ok(info) => info,
-    Err(_) => {
-      return Err(napi::Error::new(
-        napi::Status::GenericFailure,
-        "get crashpadInfo stream from dump file failed".to_owned(),
-      ))
-    }
-  };
-
-  let mut module_list: Vec<MinidumpModuleCrashpadInfo> = vec![];
-
-  // iterate the module list
-  for m in crashpad_info.module_list.iter() {
-    let module_index: u32 = m.module_index as u32;
-    let list_annotations: Vec<String> = m.list_annotations.to_owned();
-    let mut simple_annotations: HashMap<String, String> = HashMap::new();
-    let mut annotation_objects: HashMap<String, String> = HashMap::new();
-
-    for (k, v) in &m.simple_annotations {
-      simple_annotations.insert(k.to_string(), v.to_string());
-    }
-
-    for (k, v) in &m.annotation_objects {
-      match v {
-        minidump::MinidumpAnnotation::String(string) => {
-          annotation_objects.insert(k.to_string(), string.to_string());
-        }
-        minidump::MinidumpAnnotation::Invalid => {
-          annotation_objects.insert(k.to_string(), "<invalid>".to_string());
-        }
-        minidump::MinidumpAnnotation::UserDefined(_) => {
-          annotation_objects.insert(k.to_string(), "<user defined>".to_string());
-        }
-        minidump::MinidumpAnnotation::Unsupported(_) => {
-          annotation_objects.insert(k.to_string(), "<unsupported>".to_string());
-        }
-        _ => (),
-      };
-    }
-
-    let module = MinidumpModuleCrashpadInfo {
-      module_index,
-      list_annotations,
-      simple_annotations,
-      annotation_objects,
-    };
-    module_list.push(module);
-  }
-
-  Ok(CrashpadInfo { module_list })
+#[napi(object)]
+pub struct SystemInfo {
+  //
 }
 
 #[napi]
@@ -176,5 +109,22 @@ impl Minidump {
     }
 
     Ok(CrashpadInfo { module_list })
+  }
+
+  #[napi]
+  pub fn get_system_info(&self)-> napi::Result<SystemInfo> {
+    let result = &self.dump.get_stream::<minidump::MinidumpSystemInfo>();
+
+    let system_info = match result {
+      Ok(info) => info,
+      Err(_) => {
+        return Err(napi::Error::new(
+          napi::Status::GenericFailure,
+          "get systemInfo stream from dump file failed".to_owned(),
+        ))
+      }
+    };
+
+    Ok(SystemInfo {})
   }
 }
