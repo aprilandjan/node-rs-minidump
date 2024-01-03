f53c30e4dc04abf6c502da6736f08ef4874cc279
aprilandjan <merlin.ye@qq.com>
2024-01-03 23:26:31 +0800

    refactor: extract get crashpad info out of entry file

---
+++ src/crashpad_info.rs
@@ -0,0 +1,65 @@
+use napi_derive::napi;
+use std::collections::HashMap;
+use minidump;
+
+/// Additional Crashpad-specific information about a module carried within a minidump file.
+#[napi(object)]
+pub struct MinidumpModuleCrashpadInfo {
+  /// Index of the corresponding module in the `MinidumpModuleList`.
+  pub module_index: u32,
+  pub list_annotations: Vec<String>,
+  pub simple_annotations: HashMap<String, String>,
+  pub annotation_objects: HashMap<String, String>,
+}
+
+#[napi(object)]
+pub struct MinidumpCrashpadInfo {
+  pub module_list: Vec<MinidumpModuleCrashpadInfo>,
+}
+
+impl From<&minidump::MinidumpCrashpadInfo> for MinidumpCrashpadInfo {
+  fn from(crashpad_info: &minidump::MinidumpCrashpadInfo) -> Self {
+
+    let mut module_list: Vec<MinidumpModuleCrashpadInfo> = vec![];
+
+    // iterate the module list
+    for m in crashpad_info.module_list.iter() {
+      let module_index: u32 = m.module_index as u32;
+      let list_annotations: Vec<String> = m.list_annotations.to_owned();
+      let mut simple_annotations: HashMap<String, String> = HashMap::new();
+      let mut annotation_objects: HashMap<String, String> = HashMap::new();
+
+      for (k, v) in &m.simple_annotations {
+        simple_annotations.insert(k.to_string(), v.to_string());
+      }
+
+      for (k, v) in &m.annotation_objects {
+        match v {
+          minidump::MinidumpAnnotation::String(string) => {
+            annotation_objects.insert(k.to_string(), string.to_string());
+          }
+          minidump::MinidumpAnnotation::Invalid => {
+            annotation_objects.insert(k.to_string(), "<invalid>".to_string());
+          }
+          minidump::MinidumpAnnotation::UserDefined(_) => {
+            annotation_objects.insert(k.to_string(), "<user defined>".to_string());
+          }
+          minidump::MinidumpAnnotation::Unsupported(_) => {
+            annotation_objects.insert(k.to_string(), "<unsupported>".to_string());
+          }
+          _ => (),
+        };
+      }
+
+      let module = MinidumpModuleCrashpadInfo {
+        module_index,
+        list_annotations,
+        simple_annotations,
+        annotation_objects,
+      };
+      module_list.push(module);
+    }
+
+    MinidumpCrashpadInfo { module_list }
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,15 @@
 #![deny(clippy::all)]
-use std::{collections::HashMap};
 use napi_derive::napi;
 use memmap2::Mmap;
 
+use crashpad_info::MinidumpCrashpadInfo;
 use system_info::MinidumpSystemInfo;
 use misc_info::MiscInfo;
 
+// TODO: so, what's appropriate way to declare these 'mod' files?
+mod crashpad_info;
 mod system_info;
 mod misc_info;
-
-
-/// Additional Crashpad-specific information about a module carried within a minidump file.
-#[napi(object)]
-pub struct MinidumpModuleCrashpadInfo {
-  /// Index of the corresponding module in the `MinidumpModuleList`.
-  pub module_index: u32,
-  pub list_annotations: Vec<String>,
-  pub simple_annotations: HashMap<String, String>,
-  pub annotation_objects: HashMap<String, String>,
-}
-
-#[napi(object)]
-pub struct CrashpadInfo {
-  pub module_list: Vec<MinidumpModuleCrashpadInfo>,
-}
 
 #[napi]
 pub struct Minidump {
@@ -57,60 +43,13 @@ impl Minidump {
   /// instance method for napi
   /// see https://napi.rs/docs/concepts/class#class-method
   #[napi]
-  pub fn get_crashpad_info(&self)-> napi::Result<CrashpadInfo> {
-    let crashpad_info_result = &self.dump.get_stream::<minidump::MinidumpCrashpadInfo>();
+  pub fn get_crashpad_info(&self)-> napi::Result<MinidumpCrashpadInfo> {
+    let result = &self.dump.get_stream::<minidump::MinidumpCrashpadInfo>();
 
-    let crashpad_info = match crashpad_info_result {
-      Ok(info) => info,
-      Err(_) => {
-        return Err(napi::Error::new(
-          napi::Status::GenericFailure,
-          "get crashpadInfo stream from dump file failed".to_owned(),
-        ))
-      }
-    };
-
-    let mut module_list: Vec<MinidumpModuleCrashpadInfo> = vec![];
-
-    // iterate the module list
-    for m in crashpad_info.module_list.iter() {
-      let module_index: u32 = m.module_index as u32;
-      let list_annotations: Vec<String> = m.list_annotations.to_owned();
-      let mut simple_annotations: HashMap<String, String> = HashMap::new();
-      let mut annotation_objects: HashMap<String, String> = HashMap::new();
-
-      for (k, v) in &m.simple_annotations {
-        simple_annotations.insert(k.to_string(), v.to_string());
-      }
-
-      for (k, v) in &m.annotation_objects {
-        match v {
-          minidump::MinidumpAnnotation::String(string) => {
-            annotation_objects.insert(k.to_string(), string.to_string());
-          }
-          minidump::MinidumpAnnotation::Invalid => {
-            annotation_objects.insert(k.to_string(), "<invalid>".to_string());
-          }
-          minidump::MinidumpAnnotation::UserDefined(_) => {
-            annotation_objects.insert(k.to_string(), "<user defined>".to_string());
-          }
-          minidump::MinidumpAnnotation::Unsupported(_) => {
-            annotation_objects.insert(k.to_string(), "<unsupported>".to_string());
-          }
-          _ => (),
-        };
-      }
-
-      let module = MinidumpModuleCrashpadInfo {
-        module_index,
-        list_annotations,
-        simple_annotations,
-        annotation_objects,
-      };
-      module_list.push(module);
+    match result {
+      Ok(info) => Ok(MinidumpCrashpadInfo::from(info)),
+      Err(err) => Err(napi::Error::from_reason(err.to_string()))
     }
-
-    Ok(CrashpadInfo { module_list })
   }
 
   #[napi]
